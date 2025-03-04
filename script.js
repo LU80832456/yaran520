c99c31d4f06b83c4c5c19bfb36ce8db6f24d702b
LU80832456 <743344265@qq.com>
2025-03-04 10:28:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ noButton.addEventListener("click", function() {
 yesButton.addEventListener("click", function() {
     document.body.innerHTML = `
         <div class="yes-screen">
-            <h1 class="yes-text">!!!喜欢你!! ( >᎑<)♡︎ᐝ</h1>
+            <h1 class="yes-text"> ( >3<)这还差不多</h1>
             <img src="images/hug.png" alt="拥抱" class="yes-image">
         </div>
     `;
