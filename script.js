d85b8686db5bcb2fff5f84f86e1face5a04705ab
LU80832456 <743344265@qq.com>
2025-03-04 11:03:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ let clickCount = 0;  // 记录点击 No 的次数
 
 // No 按钮的文字变化
 const noTexts = [
-    "？路小染你点错了", 
-    "你真点的这个啊", 
-    "再想想！ ", 
-    "你小时候不是这么说的", 
-    "看着我的眼睛",
-    "再想想啊，路小染",
-    "不许选这个！",
+    "你按错了吧", 
+    "诶诶，我和别人打赌呢", 
+    "给我点面子呗", 
+    "输了要给技术科五十块呢（对的）", 
+    "呜呜别选这个", 
+    "不行！"
 ];
 
 // No 按钮点击事件
@@ -34,7 +33,7 @@ noButton.addEventListener("click", function() {
     questionText.style.transform = `translateY(-${moveUp}px)`;
 
     // No 文案变化（前 5 次变化）
-    if (clickCount <= 7) {
+    if (clickCount <= 6) {
         noButton.innerText = noTexts[clickCount - 1];
     }
 
@@ -51,7 +50,7 @@ noButton.addEventListener("click", function() {
 yesButton.addEventListener("click", function() {
     document.body.innerHTML = `
         <div class="yes-screen">
-            <h1 class="yes-text"> ( >3<)这还差不多</h1>
+            <h1 class="yes-text">!!!喜欢你!! ( >᎑<)♡︎ᐝ</h1>
             <img src="images/hug.png" alt="拥抱" class="yes-image">
         </div>
     `;
